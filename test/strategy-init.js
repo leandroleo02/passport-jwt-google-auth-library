eba487ef3170ccb0adb810f030ccd7c5d5631a06
Leandro Rodrigues <leandroleo02@gmail.com>
2017-04-25 19:46:20 +0100

    strategy init test has been created

---
+++ test/strategy-init.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect;
+var Strategy = require('../lib/strategy');
+
+describe('Strategy', function () {
+    var strategy = new Strategy({ jwtFromRequest: {}, clientID: 'clientID', secretOrKey: 'secret' }, () => { });
+
+    it('should be named jwt-google-auth-library', function () {
+        expect(strategy.name).to.equal('jwt-google-auth-library');
+    });
+
+    it('should throw if constructed without a verify callback', function () {
+        expect(function () {
+            var s = new Strategy({ jwtFromRequest: {}, clientID: 'clientID', secretOrKey: 'secret' });
+        }).to.throw(TypeError, "JwtGoogleAuthLibraryStrategy requires a verify callback");
+    });
+
+    it('should throw if constructed without a secretOrKey arg', function () {
+        expect(function () {
+            var s = new Strategy({ jwtFromRequest: {}, clientID: 'clientID', secretOrKey: null }, () => { });
+        }).to.throw(TypeError, 'JwtGoogleAuthLibraryStrategy requires a secret or key');
+    });
+
+    it('should throw if constructed without a clientID arg', function () {
+        expect(function () {
+            var s = new Strategy({ jwtFromRequest: {}, secretOrKey: 'secret' }, () => { });
+        }).to.throw(TypeError, 'JwtGoogleAuthLibraryStrategy requires a client ID');
+    });
+});
